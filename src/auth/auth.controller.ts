44c8095ed8884c9227a16cce957c99016bf1cf10
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-02-28 10:16:02 +0100

    enable cors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ export class AuthController {
   register(
     @Body()
     registerDto: RegisterDto
-   ) {
+    ) {
     return this.authService.register(registerDto);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
 // app.useGlobalPipes(
 //   new ValidationPipe({
 //     whitelist: true,
